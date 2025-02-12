4af75b98fb102a4acd35e63f46bad4d11949e5b2
Rafael Silva Willians <rafael.willians@outlook.com>
2025-02-12 02:14:06 +0000

    Adicionadas variaveis credenciais

--- ec2/terraform/main.tf
+++ ec2/terraform/main.tf
@@ -1,10 +1,12 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "aws_session_token" {}
 
 provider "aws" {
   region     = "us-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+  token = var.aws_session_token
 }
 
 
