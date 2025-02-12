5ddaf35bdcbc17009ce2846dbb2e9180d503e7ee
Rafael Silva Willians <rafael.willians@outlook.com>
2025-02-12 01:39:04 +0000

    Adicionados scripts

---
+++ ec2/backend.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install git -y
+sudo yum install python3 python3-pip -y
+git config --global user.name "Username"
+git config --global user.email email@email.com
+git clone https://github.com/TomasAlric/proz-back-end-saa
+cd proz-saa-backend
+pip install -r requirements.txt
+python3 server.py
---
+++ ec2/frontend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install git -y
+sudo mkdir -p /var/www/html
+sudo chmod -R 755 /var/www/html
+git config --global user.name "Username"
+git config --global user.email email@email.com
+git clone https://github.com/TomasAlric/proz-front-end-saa
+cd proz-front-end-saa
+sudo mv * /var/www/html
+sudo yum install httpd -y
