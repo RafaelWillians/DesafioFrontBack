27f78e13e637845546e092975735dad4669bf37f
Rafael Silva Willians <rafael.willians@outlook.com>
2025-02-12 01:42:07 +0000

    Adicionada info nos  scripts

--- ec2/frontend.sh
+++ ec2/frontend.sh
@@ -7,7 +7,7 @@ sudo chmod -R 755 /var/www/html
 git config --global user.name "Username"
 git config --global user.email email@email.com
 git clone https://github.com/TomasAlric/proz-front-end-saa
-cd proz-front-end-saa
+cd proz-front-end-saa/
 sudo mv * /var/www/html
 sudo yum install httpd -y
 sudo systemctl start httpd
