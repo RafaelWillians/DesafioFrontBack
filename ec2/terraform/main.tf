89edbd98bc2c3273b70b33ff8776532ac928a9ae
Rafael Silva Willians <rafael.willians@outlook.com>
2025-02-12 02:12:25 +0000

    Adicionadas variaveis credenciais

--- ec2/terraform/main.tf
+++ ec2/terraform/main.tf
@@ -1,6 +1,12 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
+
 
 resource "aws_security_group" "desafio_sg" {
   name        = "desafio-sg"
