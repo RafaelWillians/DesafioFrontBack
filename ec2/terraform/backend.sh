52748c2241bebe7ceff73aadcfc4abba87d00d16
Rafael Silva Willians <rafael.willians@outlook.com>
2025-02-12 02:24:09 +0000

    corrigido script backend

--- ec2/terraform/backend.sh
+++ ec2/terraform/backend.sh
@@ -6,6 +6,6 @@ sudo yum install python3 python3-pip -y
 git config --global user.name "Username"
 git config --global user.email email@email.com
 git clone https://github.com/TomasAlric/proz-back-end-saa
-cd proz-saa-backend
+cd proz-back-end-saa
 pip install -r requirements.txt
 python3 server.py
